5316e23fa3b8643289eaa14d17213bbd5f2c9de4
tenieceb <tabusenbark@gmail.com>
2024-09-21 17:42:47 -0600

    Completed Prep 3 assignment with stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        // Console.WriteLine("Hello Prep3 World!");
+        string replay;
+       do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess;
+            int tries = 0;
+            
+            do
+            {
+                Console.Write("What is your guess? ");
+                string input2 = Console.ReadLine();
+                guess = int.Parse(input2);
+                if (guess > magicNumber )
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber )
+                {
+                    Console.WriteLine("Higher");
+                }
+                tries += 1;
+            }while (guess != magicNumber);
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {tries} guess(es) to beat the game!");
+            }
+
+            Console.Write("Would you like to play again? ");
+            replay = Console.ReadLine();
+        } while(replay == "yes");
+    }    
 }
