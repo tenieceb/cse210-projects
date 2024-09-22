fe09fb41a29736ec68f7bf3b688fa188898893d4
tenieceb <tabusenbark@gmail.com>
2024-09-21 18:49:08 -0600

    Completed prep 4 assignment, no stretch challenge

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers= new List<int>();
+
+        int inputNumber;
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            inputNumber = int.Parse(input);
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        } while (inputNumber != 0);
+
+        int listSum = numbers.Sum();
+        float listCount = numbers.Count();
+        float listAverage = listSum / listCount;
+        int listMax = numbers.Max();
+
+        Console.WriteLine($"The sum is: {listSum}");
+        Console.WriteLine($"The average is: {listAverage}");
+        Console.WriteLine($"The largest number is: {listMax}");
+
+
     }
 }
