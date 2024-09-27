fbffb90ba46e22bf3dbe19dee2b025baf47c5e71
tenieceb <tabusenbark@gmail.com>
2024-09-27 16:57:01 -0600

    Moved defined function out of the Main function to get better habits formed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -9,40 +9,7 @@ class Program
         // Console.WriteLine("Hello Prep5 World!");
 
         // DisplayWelcome - Displays the message, "Welcome to the Program!"
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
 
-        // PromptUserName - Asks for and returns the user's name (as a string)
-        static string PromptUserName()
-        {
-            Console.Write("Please enter your name: ");
-            string userName = Console.ReadLine();
-            return userName;
-        }
-
-        // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
-        static int PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            string numberInput = Console.ReadLine();
-            int userNumber = int.Parse(numberInput);
-            return userNumber;
-        }
-
-        // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
-       static int SquareNumber(int userNumber)
-       {
-        int numberSquared = userNumber * userNumber;
-        return numberSquared;
-       } 
-
-        // DisplayResult - Accepts the user's name and the squared number and displays them.
-       static void DisplayResult(string userName, int numberSquared)
-       {
-        Console.WriteLine($"{userName}, the square of your number is {numberSquared}.");
-       }
 
         DisplayWelcome();
         string userName = PromptUserName();
@@ -54,5 +21,39 @@ class Program
 
        DisplayResult(userName, numberSquared);
     
+}
+        static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
     }
+
+        // PromptUserName - Asks for and returns the user's name (as a string)
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+        // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string numberInput = Console.ReadLine();
+        int userNumber = int.Parse(numberInput);
+        return userNumber;
+    }
+
+        // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    static int SquareNumber(int userNumber)
+    {
+    int numberSquared = userNumber * userNumber;
+    return numberSquared;
+    } 
+
+        // DisplayResult - Accepts the user's name and the squared number and displays them.
+    static void DisplayResult(string userName, int numberSquared)
+    {
+    Console.WriteLine($"{userName}, the square of your number is {numberSquared}.");
+    }      
 }
