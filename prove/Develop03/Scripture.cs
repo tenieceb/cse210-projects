f518c34924bb36f1bac6ab3e4a13fb54667b061b
tenieceb <tabusenbark@gmail.com>
2024-10-04 15:56:53 -0600

    Worked on Scripture Class

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,31 +1,41 @@
-public class Scripture
-{
-    Reference _reference;
-    public List<Word> _words = new List<Word>();
-
-    public Scripture(Reference reference, string text)
+    public class Scripture
     {
-        _reference = reference;
-        text = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life";
-        char [] separator = {' ', ',' };
-        string [] textArray = (text.Split(separator));
-        _words.Add(textArray);
-        
+        Reference _reference;
+        public List<Word> _words = new List<Word>();
+
+        public Scripture(Reference reference, string text)
+        {
+            _reference = reference;
+            text = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life";
+            string[] textArray = text.Split(' ',',',';');
+            foreach (string line in textArray)
+            {
+                _words.Add(new Word(line));
+            }
+        }
+
+        public void HideRandomWord(int numberToHide)
+        {
+            Random random = new Random();
+            numberToHide = random.Next(_words.Count);
+            string toHide = numberToHide.ToString();
+
+            Word hidden = new Word(toHide);
+            hidden.Hide();
+            
+
+        }
+
+        public string GetAllText()
+        {
+            
+        }
+
+        public bool IsCompletelyHidden()
+        { }
+
+
 
 
     }
-
-    public Scripture() { }
-
-    public void HideRandomWord(int numberToHide)
-    {
-
-    }
-
-    public string GetAllText()
-    {
-        
-    }
-
-    public bool IsCompletelyHidden()
-    { }
+}
